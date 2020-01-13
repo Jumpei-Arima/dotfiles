9d4c6ee31595984fd856e8f1ad770591d338e448
arima <arijun0307@gmail.com>
2020-01-13 20:42:58 +0900

    update intall.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
+mkdir $SCRIPT_DIR/vim/rc/plugins
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > $SCRIPT_DIR/vim/rc/plugins/installer.sh
 
 sh $SCRIPT_DIR/vim/rc/plugins/installer.sh $SCRIPT_DIR/vim/rc/plugins
