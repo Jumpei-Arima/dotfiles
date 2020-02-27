dfc901fda9e66e41f6e1b740da61763418f0d022
arima <arijun0307@gmail.com>
2020-02-27 23:09:22 +0900

    add undo

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 mkdir $SCRIPT_DIR/vim/rc/plugins
+mkdir $SCRIPT_DIR/vim/undo
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > $SCRIPT_DIR/vim/rc/plugins/installer.sh
 
 sh $SCRIPT_DIR/vim/rc/plugins/installer.sh $SCRIPT_DIR/vim/rc/plugins
