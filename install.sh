da7c476aa597c83fa576fbf86b903746f46fd96a
arima <arijun0307@gmail.com>
2026-05-10 21:49:36 +0900

    delete old configs

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
-
-mkdir $SCRIPT_DIR/vim/rc/plugins
-mkdir $SCRIPT_DIR/vim/undo
-curl https://raw.githubusercontent.com/Shougo/dein-installer.vim/master/installer.sh > $SCRIPT_DIR/vim/rc/plugins/installer.sh
-
-sh $SCRIPT_DIR/vim/rc/plugins/installer.sh $SCRIPT_DIR/vim/rc/plugins
